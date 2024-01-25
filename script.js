da4aa9994e6917356a96ab1a82fa70b22ee4f7af
HARIHARANATH <154905590+Hari-hara7@users.noreply.github.com>
2024-01-25 20:04:32 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,5 @@
+ // JavaScript function for starting challenges
+ function startChallenge(challengeLink) {
+    // Redirect to the specified challenge link
+    window.location.href = challengeLink;
+}
